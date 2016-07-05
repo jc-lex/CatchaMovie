58754c9a61736fad6e2921f5284a8f9e484c52ca
radman13666 <williamkaos.kibirango76@gmail.com>
2016-07-05 17:44:05 +0300

    added GridView for movie posters

--- app/src/main/java/com/radindustries/radman/catchamovie/MainActivityFragment.java
+++ app/src/main/java/com/radindustries/radman/catchamovie/MainActivityFragment.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -17,6 +20,12 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        //creating an ArrayList of images to use to populate the GridView
+        ArrayList<ImageView> posters = new ArrayList<>();
+
+
+        return rootView;
     }
 }
