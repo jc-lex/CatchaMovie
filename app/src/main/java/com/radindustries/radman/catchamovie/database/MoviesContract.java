df7640b93a8fbf4036966bf9ce2985d10463cf61
radman13666 <williamkaos.kibirango76@gmail.com>
2016-07-20 16:31:00 +0300

    added some utilty methods to MovieContract and implemented the MovieProvider

--- app/src/main/java/com/radindustries/radman/catchamovie/database/MoviesContract.java
+++ app/src/main/java/com/radindustries/radman/catchamovie/database/MoviesContract.java
@@ -1,6 +1,7 @@
 package com.radindustries.radman.catchamovie.database;
 
 import android.content.ContentResolver;
+import android.content.ContentUris;
 import android.net.Uri;
 import android.provider.BaseColumns;
 
@@ -9,7 +10,7 @@ import android.provider.BaseColumns;
  */
 public class MoviesContract {
 
-    public static final String CONTENT_AUTHORITY = "com.radindustries.radman.catchamovie.provider";
+    public static final String CONTENT_AUTHORITY = "com.radindustries.radman.catchamovie.database";
     public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
 
     public static final String PATH_MOVIES = "movies";
@@ -20,7 +21,7 @@ public class MoviesContract {
 
         public static final Uri CONTENT_URI =
                 BASE_CONTENT_URI.buildUpon().appendPath(PATH_MOVIES).build();
-        public static final String CONTENT_TYPE =
+        public static final String CONTENT_DIR_TYPE =
                 ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_MOVIES;
         public static final String CONTENT_ITEM_TYPE =
                 ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_MOVIES;
@@ -36,16 +37,50 @@ public class MoviesContract {
         public static final String COL_MOVIE_SORT_TYPE_SETTING = "movie_sort_type_setting";
         public static final String COL_IS_FAVOURITE = "is_favourite";
 
+        public static Uri buildMovieUri(long id) {
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
+
+        public static Uri buildMovieUriWithSortTypeAndId(String sortType, int id) {
+            return CONTENT_URI.buildUpon().appendPath(sortType)
+                    .appendPath(Integer.toString(id)).build();
+        }
+
+        public static Uri buildMovieUriWithSortType(String sortType) {
+            return CONTENT_URI.buildUpon().appendPath(sortType).build();
+        }
+
+        public static Uri buildMovieUriWithIsFavSetting(int fav) {
+            return CONTENT_URI.buildUpon().appendPath(Integer.toString(fav)).build();
+        }
+
+        public static Uri buildMovieUriWithIsFavSettingAndId(int fav, int id) {
+            return CONTENT_URI.buildUpon().appendPath(Integer.toString(fav))
+                    .appendPath(Integer.toString(id)).build();
+        }
+
+        public static String getSortTypeSettingFromUri(Uri uri) {
+            return uri.getPathSegments().get(1);
+        }
+
+        public static int getIsFavSettingFromUri(Uri uri) {
+            return Integer.parseInt(uri.getPathSegments().get(1));
+        }
+
+        public static int getMovieIdFromUri(Uri uri) {
+            return Integer.parseInt(uri.getPathSegments().get(2));
+        }
+
     }
 
     public static final class TrailerEntry implements BaseColumns {
 
         public static final Uri CONTENT_URI =
                 BASE_CONTENT_URI.buildUpon().appendPath(PATH_TRAILERS).build();
-        public static final String CONTENT_TYPE =
+        public static final String CONTENT_DIR_TYPE =
                 ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_TRAILERS;
-        public static final String CONTENT_ITEM_TYPE =
-                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_TRAILERS;
+//        public static final String CONTENT_ITEM_TYPE =
+//                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_TRAILERS;
 
         //table name and columns
         public static final String TABLE_NAME = "trailers";
@@ -53,22 +88,38 @@ public class MoviesContract {
         public static final String COL_MOVIE_TRAILER_NAME = "movie_trailer_name";
         public static final String COL_MOVIE_TRAILER_URL = "movie_trailer_url";
 
+        public static Uri buildTrailerUri(long id) {
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
+
+        public static Uri buildTrailerUriWithId(int id) {
+            return CONTENT_URI.buildUpon().appendPath(Integer.toString(id)).build();
+        }
+
     }
 
     public static final class ReviewEntry implements BaseColumns {
 
         public static final Uri CONTENT_URI =
                 BASE_CONTENT_URI.buildUpon().appendPath(PATH_REVIEWS).build();
-        public static final String CONTENT_TYPE =
+        public static final String CONTENT_DIR_TYPE =
                 ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_REVIEWS;
-        public static final String CONTENT_ITEM_TYPE =
-                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_REVIEWS;
+//        public static final String CONTENT_ITEM_TYPE =
+//                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_REVIEWS;
 
         //table name and columns
         public static final String TABLE_NAME = "reviews";
         public static final String COL_MOVIE_ID = "movie_id";
         public static final String COL_MOVIE_REVIEW_AUTHOR = "movie_review_author";
         public static final String COL_MOVIE_REVIEW = "movie_review";
+
+        public static Uri buildReviewUri(long id) {
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
+
+        public static Uri buidReviewUriWithId(int id) {
+            return CONTENT_URI.buildUpon().appendPath(Integer.toString(id)).build();
+        }
 
     }
 
---
+++ app/src/main/java/com/radindustries/radman/catchamovie/database/MoviesProvider.java
@@ -0,0 +1,299 @@
+package com.radindustries.radman.catchamovie.database;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+
+public class MoviesProvider extends ContentProvider {
+
+    private MovieDBHelper movieDBHelper;
+    private static final UriMatcher matcher = buildUriMatcher();
+
+    public static final int MOVIES = 100;
+    public static final int MOVIES_WITH_SORT_TYPE = 200;
+    public static final int MOVIES_WITH_FAV = 300;
+    public static final int MOVIES_WITH_SORT_TYPE_AND_ID = 400;
+    public static final int MOVIES_WITH_FAV_AND_ID = 500;
+    public static final int TRAILERS = 600;
+    public static final int TRAILERS_WITH_ID = 700;
+    public static final int REVIEWS = 800;
+    public static final int REVIEWS_WITH_ID = 900;
+
+//    private static final SQLiteQueryBuilder mTrailerByMovieIdQueryBuilder;
+//    private static final SQLiteQueryBuilder mReviewByMovieIdQueryBuilder;
+//
+//    static {
+//        mTrailerByMovieIdQueryBuilder = new SQLiteQueryBuilder();
+//        mReviewByMovieIdQueryBuilder = new SQLiteQueryBuilder();
+//
+//        mTrailerByMovieIdQueryBuilder.setTables(
+//                MoviesContract.TrailerEntry.TABLE_NAME + " INNER JOIN " +
+//                        MoviesContract.MoviesEntry.TABLE_NAME + " ON " +
+//                        MoviesContract.TrailerEntry.TABLE_NAME + "." +
+//                        MoviesContract.TrailerEntry.COL_MOVIE_ID + " = " +
+//                        MoviesContract.MoviesEntry.TABLE_NAME + "." +
+//                        MoviesContract.MoviesEntry.COL_MOVIE_ID
+//        );
+//        mReviewByMovieIdQueryBuilder.setTables(
+//                MoviesContract.ReviewEntry.TABLE_NAME + " INNER JOIN " +
+//                        MoviesContract.MoviesEntry.TABLE_NAME + " ON " +
+//                        MoviesContract.ReviewEntry.TABLE_NAME + "." +
+//                        MoviesContract.ReviewEntry.COL_MOVIE_ID + " = " +
+//                        MoviesContract.MoviesEntry.TABLE_NAME + "." +
+//                        MoviesContract.MoviesEntry.COL_MOVIE_ID
+//        );
+//    }
+
+    public MoviesProvider() {
+    }
+
+    @Override
+    public int delete(@NonNull Uri uri, String selection, String[] selectionArgs) {
+        // Implement this to handle requests to delete one or more rows.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    public static UriMatcher buildUriMatcher() {
+
+        final UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+        final String AUTHORITY = MoviesContract.CONTENT_AUTHORITY;
+
+        uriMatcher.addURI(AUTHORITY, MoviesContract.PATH_MOVIES, MOVIES);
+        uriMatcher.addURI(AUTHORITY, MoviesContract.PATH_MOVIES + "/*", MOVIES_WITH_SORT_TYPE);
+        uriMatcher.addURI(AUTHORITY, MoviesContract.PATH_MOVIES + "/#", MOVIES_WITH_FAV);
+        uriMatcher.addURI(AUTHORITY, MoviesContract.PATH_MOVIES + "/*/#", MOVIES_WITH_SORT_TYPE_AND_ID);
+        uriMatcher.addURI(AUTHORITY, MoviesContract.PATH_MOVIES + "/#/#", MOVIES_WITH_FAV_AND_ID);
+        uriMatcher.addURI(AUTHORITY, MoviesContract.PATH_TRAILERS, TRAILERS);
+        uriMatcher.addURI(AUTHORITY, MoviesContract.PATH_TRAILERS + "/#", TRAILERS_WITH_ID);
+        uriMatcher.addURI(AUTHORITY, MoviesContract.PATH_REVIEWS, REVIEWS);
+        uriMatcher.addURI(AUTHORITY, MoviesContract.PATH_REVIEWS + "/#", REVIEWS_WITH_ID);
+
+        return uriMatcher;
+    }
+
+    @Override
+    public String getType(@NonNull Uri uri) {
+
+        final int match = matcher.match(uri);
+
+        switch (match) {
+            case MOVIES: return MoviesContract.MoviesEntry.CONTENT_DIR_TYPE;
+            case MOVIES_WITH_SORT_TYPE: return MoviesContract.MoviesEntry.CONTENT_DIR_TYPE;
+            case MOVIES_WITH_FAV: return MoviesContract.MoviesEntry.CONTENT_DIR_TYPE;
+            case MOVIES_WITH_SORT_TYPE_AND_ID: return MoviesContract.MoviesEntry.CONTENT_ITEM_TYPE;
+            case MOVIES_WITH_FAV_AND_ID: return MoviesContract.MoviesEntry.CONTENT_ITEM_TYPE;
+            case TRAILERS: return MoviesContract.TrailerEntry.CONTENT_DIR_TYPE;
+            case TRAILERS_WITH_ID: return MoviesContract.TrailerEntry.CONTENT_DIR_TYPE;
+            case REVIEWS: return MoviesContract.ReviewEntry.CONTENT_DIR_TYPE;
+            case REVIEWS_WITH_ID: return MoviesContract.ReviewEntry.CONTENT_DIR_TYPE;
+            default: throw new UnsupportedOperationException("Uknown Uri: " + uri);
+        }
+
+    }
+
+    @Override
+    public Uri insert(@NonNull Uri uri, ContentValues values) {
+
+        final SQLiteDatabase db = movieDBHelper.getWritableDatabase();
+        final int match = matcher.match(uri);
+        Uri resultUri;
+
+        switch (match) {
+            case MOVIES:
+                long movieId = db.insert(MoviesContract.MoviesEntry.TABLE_NAME, null, values);
+                if (movieId > 0) resultUri = MoviesContract.MoviesEntry.buildMovieUri(movieId);
+                else throw new SQLException("Failed to insert row into " + uri);
+                break;
+            case TRAILERS:
+                long trailerId = db.insert(MoviesContract.TrailerEntry.TABLE_NAME,
+                        MoviesContract.TrailerEntry.COL_MOVIE_TRAILER_URL, values);
+                if (trailerId > 0) resultUri = MoviesContract.TrailerEntry.buildTrailerUri(trailerId);
+                else throw new SQLException("Failed to insert row into " + uri);
+                break;
+            case REVIEWS:
+                long reviewId = db.insert(MoviesContract.ReviewEntry.TABLE_NAME,
+                        MoviesContract.ReviewEntry.COL_MOVIE_REVIEW, values);
+                if (reviewId > 0) resultUri = MoviesContract.ReviewEntry.buildReviewUri(reviewId);
+                else throw new SQLException("Failed to insert row into " + uri);
+                break;
+            default: throw new UnsupportedOperationException("Unknown Uri: " + uri);
+        }
+        getContext().getContentResolver().notifyChange(uri, null);
+        return resultUri;
+
+    }
+
+    @Override
+    public boolean onCreate() {
+        movieDBHelper = new MovieDBHelper(getContext());
+        return true;
+    }
+
+    @Override
+    public Cursor query(@NonNull Uri uri, String[] projection, String selection,
+                        String[] selectionArgs, String sortOrder) {
+
+        SQLiteDatabase db = movieDBHelper.getReadableDatabase();
+        Cursor returnedCursor;
+        switch (matcher.match(uri)) {
+            case MOVIES_WITH_SORT_TYPE:
+                String sortType = MoviesContract.MoviesEntry.getSortTypeSettingFromUri(uri);
+                projection = new String[]{MoviesContract.MoviesEntry.COL_MOVIE_ID,
+                        MoviesContract.MoviesEntry.COL_MOVIE_POSTER_URL};
+                selection = MoviesContract.MoviesEntry.COL_MOVIE_SORT_TYPE_SETTING + " = ? ";
+                selectionArgs = new String[]{sortType};
+                returnedCursor = db.query(true, MoviesContract.MoviesEntry.TABLE_NAME,
+                        projection, selection, selectionArgs, null, null, null, "20"); break;
+            case MOVIES_WITH_FAV:
+                String fav = Integer.toString(MoviesContract.MoviesEntry.getIsFavSettingFromUri(uri));
+                projection = new String[]{MoviesContract.MoviesEntry.COL_MOVIE_ID,
+                        MoviesContract.MoviesEntry.COL_MOVIE_POSTER_URL};
+                selection = MoviesContract.MoviesEntry.COL_IS_FAVOURITE + " = ? ";
+                selectionArgs = new String[]{fav};
+                returnedCursor = db.query(true, MoviesContract.MoviesEntry.TABLE_NAME,
+                        projection, selection, selectionArgs, null, null, null, null); break;
+            case MOVIES_WITH_SORT_TYPE_AND_ID:
+                String sortType1 = MoviesContract.MoviesEntry.getSortTypeSettingFromUri(uri);
+                String id = Integer.toString(MoviesContract.MoviesEntry.getMovieIdFromUri(uri));
+                projection = new String[]{
+                        MoviesContract.MoviesEntry.COL_MOVIE_TITLE,
+                        MoviesContract.MoviesEntry.COL_MOVIE_POSTER_URL,
+                        MoviesContract.MoviesEntry.COL_MOVIE_RELEASE_DATE,
+                        MoviesContract.MoviesEntry.COL_MOVIE_USER_RATING,
+                        MoviesContract.MoviesEntry.COL_MOVIE_PLOT_SYNOPSIS
+                };
+                selection = MoviesContract.MoviesEntry.COL_MOVIE_ID + " = ? AND " +
+                        MoviesContract.MoviesEntry.COL_MOVIE_SORT_TYPE_SETTING + " = ? ";
+                selectionArgs = new String[]{id, sortType1};
+                returnedCursor = db.query(MoviesContract.MoviesEntry.TABLE_NAME,
+                        projection, selection, selectionArgs, null, null, null); break;
+            case MOVIES_WITH_FAV_AND_ID:
+                String fav1 = Integer.toString(MoviesContract.MoviesEntry.getIsFavSettingFromUri(uri));
+                String id1 = Integer.toString(MoviesContract.MoviesEntry.getMovieIdFromUri(uri));
+                projection = new String[]{
+                        MoviesContract.MoviesEntry.COL_MOVIE_TITLE,
+                        MoviesContract.MoviesEntry.COL_MOVIE_POSTER_URL,
+                        MoviesContract.MoviesEntry.COL_MOVIE_RELEASE_DATE,
+                        MoviesContract.MoviesEntry.COL_MOVIE_USER_RATING,
+                        MoviesContract.MoviesEntry.COL_MOVIE_PLOT_SYNOPSIS
+                };
+                selection = MoviesContract.MoviesEntry.COL_MOVIE_ID + " = ? AND " +
+                        MoviesContract.MoviesEntry.COL_IS_FAVOURITE + " = ? ";
+                selectionArgs = new String[]{id1, fav1};
+                returnedCursor = db.query(MoviesContract.MoviesEntry.TABLE_NAME,
+                        projection, selection, selectionArgs, null, null, null); break;
+            case TRAILERS_WITH_ID:
+                String id2 = Integer.toString(MoviesContract.MoviesEntry.getMovieIdFromUri(uri));
+                projection = new String[]{
+                        MoviesContract.TrailerEntry.COL_MOVIE_TRAILER_NAME,
+                        MoviesContract.TrailerEntry.COL_MOVIE_TRAILER_URL
+                };
+                selection = MoviesContract.TrailerEntry.COL_MOVIE_ID + " = ? ";
+                selectionArgs = new String[]{id2};
+                returnedCursor = db.query(MoviesContract.TrailerEntry.TABLE_NAME,
+                        projection, selection, selectionArgs, null, null, null); break;
+            case REVIEWS_WITH_ID:
+                String id3 = Integer.toString(MoviesContract.MoviesEntry.getMovieIdFromUri(uri));
+                projection = new String[]{
+                        MoviesContract.ReviewEntry.COL_MOVIE_REVIEW_AUTHOR,
+                        MoviesContract.ReviewEntry.COL_MOVIE_REVIEW
+                };
+                selection = MoviesContract.ReviewEntry.COL_MOVIE_ID + " = ? ";
+                selectionArgs = new String[]{id3};
+                returnedCursor = db.query(MoviesContract.ReviewEntry.TABLE_NAME,
+                        projection, selection, selectionArgs, null, null, null); break;
+            default: throw new UnsupportedOperationException("Unknown Uri: " + uri);
+        }
+        returnedCursor.setNotificationUri(getContext().getContentResolver(), uri);
+        return returnedCursor;
+
+    }
+
+    @Override
+    public int update(@NonNull Uri uri, ContentValues values, String selection,
+                      String[] selectionArgs) {
+
+        final SQLiteDatabase db = movieDBHelper.getWritableDatabase();
+        final int match = matcher.match(uri);
+        int rowsUpdated;
+        switch (match) {
+            case MOVIES_WITH_SORT_TYPE_AND_ID:
+                values.put(MoviesContract.MoviesEntry.COL_IS_FAVOURITE, 1);
+                String id = Integer.toString(MoviesContract.MoviesEntry.getMovieIdFromUri(uri));
+                selection = MoviesContract.MoviesEntry.COL_MOVIE_ID + " = ? ";
+                selectionArgs = new String[]{id};
+                rowsUpdated = db.update(MoviesContract.MoviesEntry.TABLE_NAME,
+                        values, selection, selectionArgs); break;
+            case MOVIES_WITH_FAV_AND_ID:
+                values.put(MoviesContract.MoviesEntry.COL_IS_FAVOURITE, 0);
+                String id1 = Integer.toString(MoviesContract.MoviesEntry.getMovieIdFromUri(uri));
+                selection = MoviesContract.MoviesEntry.COL_MOVIE_ID + " = ? ";
+                selectionArgs = new String[]{id1};
+                rowsUpdated = db.update(MoviesContract.MoviesEntry.TABLE_NAME,
+                        values, selection, selectionArgs); break;
+            default: throw new UnsupportedOperationException("Unknown Uri: " + uri);
+        }
+        if (rowsUpdated != 0) {
+            getContext().getContentResolver().notifyChange(uri, null);
+        }
+        return rowsUpdated;
+    }
+
+    @Override
+    public int bulkInsert(@NonNull Uri uri, @NonNull ContentValues[] values) {
+
+        final SQLiteDatabase db = movieDBHelper.getWritableDatabase();
+        final int match = matcher.match(uri);
+        int returnCount;
+
+        switch (match) {
+            case MOVIES:
+                db.beginTransaction();
+                returnCount = 0;
+                try {
+                    for (ContentValues value : values) {
+                        long movie_id = db.insert(MoviesContract.MoviesEntry.TABLE_NAME, null, value);
+                        if (movie_id != -1) returnCount++;
+                    }
+                    db.setTransactionSuccessful();
+                } finally {
+                    db.endTransaction();
+                } break;
+            case TRAILERS:
+                db.beginTransaction();
+                returnCount = 0;
+                try {
+                    for (ContentValues value : values) {
+                        long trailer_id = db.insert(MoviesContract.TrailerEntry.TABLE_NAME,
+                                MoviesContract.TrailerEntry.COL_MOVIE_TRAILER_URL ,value);
+                        if (trailer_id != -1) returnCount++;
+                    }
+                    db.setTransactionSuccessful();
+                } finally {
+                    db.endTransaction();
+                } break;
+            case REVIEWS:
+                db.beginTransaction();
+                returnCount = 0;
+                try {
+                    for (ContentValues value : values) {
+                        long review_id = db.insert(MoviesContract.ReviewEntry.TABLE_NAME,
+                                MoviesContract.ReviewEntry.COL_MOVIE_REVIEW, value);
+                        if (review_id != -1) returnCount++;
+                    }
+                    db.setTransactionSuccessful();
+                } finally {
+                    db.endTransaction();
+                } break;
+            default: return super.bulkInsert(uri, values);
+        }
+        getContext().getContentResolver().notifyChange(uri, null);
+        return returnCount;
+
+    }
+}
