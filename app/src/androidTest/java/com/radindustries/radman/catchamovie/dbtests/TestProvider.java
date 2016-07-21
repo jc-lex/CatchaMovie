8bba6f8be8ccb94c0b48d42fa24383af1fbcf9bd
radman13666 <williamkaos.kibirango76@gmail.com>
2016-07-21 17:04:17 +0300

    added tests for inserting using the provider, it works

--- app/src/androidTest/java/com/radindustries/radman/catchamovie/dbtests/TestProvider.java
+++ app/src/androidTest/java/com/radindustries/radman/catchamovie/dbtests/TestProvider.java
@@ -1,11 +1,13 @@
 package com.radindustries.radman.catchamovie.dbtests;
 
 import android.content.ComponentName;
+import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.pm.PackageManager;
 import android.content.pm.ProviderInfo;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.test.AndroidTestCase;
 
 import com.radindustries.radman.catchamovie.database.MovieDBHelper;
@@ -298,6 +300,160 @@ public class TestProvider extends AndroidTestCase {
 //        Cursor cursor = mContext.getContentResolver().query(
 //                MoviesContract.ReviewEntry.CONTENT_URI, null, null, null, null
 //        );
+
+        Cursor cursor = mContext.getContentResolver().query(
+                MoviesContract.ReviewEntry.CONTENT_URI,
+                new String[]{
+                        MoviesContract.ReviewEntry.COL_MOVIE_REVIEW_AUTHOR,
+                        MoviesContract.ReviewEntry.COL_MOVIE_REVIEW
+                },
+                MoviesContract.ReviewEntry.COL_MOVIE_ID + " = ? ",
+                new String[] {"5964"},
+                null
+        );
+        assertTrue("Error: could not get a row of data back", cursor.moveToFirst());
+        assertTrue(cursor.getCount() == 1);
+        assertTrue(cursor.getColumnCount() == 2);
+
+        //int id = cursor.getInt(cursor.getColumnIndex(MoviesContract.ReviewEntry.COL_MOVIE_ID));
+        String author = cursor.getString(cursor
+                .getColumnIndex(MoviesContract.ReviewEntry.COL_MOVIE_REVIEW_AUTHOR));
+        String review = cursor.getString(cursor.getColumnIndex(MoviesContract.ReviewEntry.COL_MOVIE_REVIEW));
+
+        //assertEquals(5964, id);
+        assertEquals("Sir. Hesketh Bell", author);
+        assertEquals("it was the best movie of all time!", review);
+
+        cursor.close();
+        db.close();
+
+    }
+
+    public void testMovieInsert() {
+
+        SQLiteDatabase db = movieDBHelper.getWritableDatabase();
+        assertTrue("Error: the db is not open", db.isOpen());
+
+        ContentValues cv = new ContentValues();
+        cv.put(MoviesContract.MoviesEntry.COL_MOVIE_TITLE, "Batman Vs Superman");
+        cv.put(MoviesContract.MoviesEntry.COL_MOVIE_POSTER_URL,
+                "http://www.image.com/alsbravierhnv.jpg");
+        cv.put(MoviesContract.MoviesEntry.COL_MOVIE_RELEASE_DATE, "3 Jul 2013");
+        cv.put(MoviesContract.MoviesEntry.COL_MOVIE_USER_RATING, "7 / 10");
+        cv.put(MoviesContract.MoviesEntry.COL_MOVIE_PLOT_SYNOPSIS,
+                "It was a dark and stormy night...");
+        cv.put(MoviesContract.MoviesEntry.COL_MOVIE_SORT_TYPE_SETTING, "popular");
+        cv.put(MoviesContract.MoviesEntry.COL_IS_FAVOURITE, 1);
+        cv.put(MoviesContract.MoviesEntry.COL_MOVIE_ID, 6785);
+
+        Uri movieUri = mContext.getContentResolver()
+                .insert(MoviesContract.MoviesEntry.CONTENT_URI, cv);
+        assertTrue(movieUri != null);
+
+        long _id = ContentUris.parseId(movieUri);
+        assertTrue(_id != -1);
+
+        Cursor cursor = mContext.getContentResolver().query(
+                MoviesContract.MoviesEntry.CONTENT_URI,
+                new String[] {
+                        MoviesContract.MoviesEntry.COL_MOVIE_TITLE,
+                        MoviesContract.MoviesEntry.COL_MOVIE_POSTER_URL,
+                        MoviesContract.MoviesEntry.COL_MOVIE_RELEASE_DATE,
+                        MoviesContract.MoviesEntry.COL_MOVIE_USER_RATING,
+                        MoviesContract.MoviesEntry.COL_MOVIE_PLOT_SYNOPSIS
+                },
+                MoviesContract.MoviesEntry.COL_MOVIE_ID + " = ? AND " +
+                        MoviesContract.MoviesEntry.COL_IS_FAVOURITE + " = ? ",
+                new String[] {"6785", "1"},
+                null
+        );
+        assertTrue("Error: no data result set received", cursor.moveToFirst());
+        assertTrue(cursor.getCount() == 1);
+        assertTrue(cursor.getColumnCount() == 5);
+
+        String title = cursor.getString(cursor
+                .getColumnIndex(MoviesContract.MoviesEntry.COL_MOVIE_TITLE));
+        String posterUrl = cursor.getString(cursor
+                .getColumnIndex(MoviesContract.MoviesEntry.COL_MOVIE_POSTER_URL));
+        String releaseDate = cursor.getString(cursor
+                .getColumnIndex(MoviesContract.MoviesEntry.COL_MOVIE_RELEASE_DATE));
+        String plot = cursor.getString(cursor
+                .getColumnIndex(MoviesContract.MoviesEntry.COL_MOVIE_PLOT_SYNOPSIS));
+        String rating = cursor.getString(cursor
+                .getColumnIndex(MoviesContract.MoviesEntry.COL_MOVIE_USER_RATING));
+
+        assertEquals("Batman Vs Superman", title);
+        assertEquals("http://www.image.com/alsbravierhnv.jpg", posterUrl);
+        assertEquals("3 Jul 2013", releaseDate);
+        assertEquals("It was a dark and stormy night...", plot);
+        assertEquals("7 / 10", rating);
+
+        cursor.close();
+        db.close();
+
+    }
+
+    public void testTrailerInsert() {
+
+        SQLiteDatabase db = movieDBHelper.getWritableDatabase();
+        assertTrue("Error: db couldn't be opened", db.isOpen());
+
+        ContentValues cv = new ContentValues();
+        cv.put(MoviesContract.TrailerEntry.COL_MOVIE_ID, 5986);
+        cv.put(MoviesContract.TrailerEntry.COL_MOVIE_TRAILER_NAME, "Trailer 1");
+        cv.put(MoviesContract.TrailerEntry.COL_MOVIE_TRAILER_URL,
+                "http://www.youtube.com/watch?v=96599yfw");
+
+        Uri trailerUri = mContext.getContentResolver().insert(MoviesContract.TrailerEntry.CONTENT_URI, cv);
+        assertTrue(trailerUri != null);
+
+        long _id = ContentUris.parseId(trailerUri);
+        assertTrue(_id != -1);
+
+        Cursor cursor = mContext.getContentResolver().query(
+                MoviesContract.TrailerEntry.CONTENT_URI,
+                new String[]{
+                        MoviesContract.TrailerEntry.COL_MOVIE_TRAILER_NAME,
+                        MoviesContract.TrailerEntry.COL_MOVIE_TRAILER_URL
+                },
+                MoviesContract.TrailerEntry.COL_MOVIE_ID + " = ? ",
+                new String[] {"5986"},
+                null
+        );
+        assertTrue("Error: could not get results from the table", cursor.moveToFirst());
+        assertTrue(cursor.getCount() == 1);
+        assertTrue(cursor.getColumnCount() == 2);
+
+        //int id = cursor.getInt(cursor.getColumnIndex(MoviesContract.TrailerEntry.COL_MOVIE_ID));
+        String name = cursor.getString(cursor
+                .getColumnIndex(MoviesContract.TrailerEntry.COL_MOVIE_TRAILER_NAME));
+        String url = cursor.getString(cursor
+                .getColumnIndex(MoviesContract.TrailerEntry.COL_MOVIE_TRAILER_URL));
+
+        //assertEquals(5986, id);
+        assertEquals("Trailer 1", name);
+        assertEquals("http://www.youtube.com/watch?v=96599yfw", url);
+
+        cursor.close();
+        db.close();
+
+    }
+
+    public void testReviewInsert() {
+
+        SQLiteDatabase db = movieDBHelper.getWritableDatabase();
+        assertTrue("Error: db could not be opened", db.isOpen());
+
+        ContentValues cv = new ContentValues();
+        cv.put(MoviesContract.ReviewEntry.COL_MOVIE_ID, 5964);
+        cv.put(MoviesContract.ReviewEntry.COL_MOVIE_REVIEW_AUTHOR, "Sir. Hesketh Bell");
+        cv.put(MoviesContract.ReviewEntry.COL_MOVIE_REVIEW, "it was the best movie of all time!");
+
+        Uri reviewUri = mContext.getContentResolver().insert(MoviesContract.ReviewEntry.CONTENT_URI, cv);
+        assertTrue(reviewUri != null);
+
+        long _id = ContentUris.parseId(reviewUri);
+        assertTrue(_id != -1);
 
         Cursor cursor = mContext.getContentResolver().query(
                 MoviesContract.ReviewEntry.CONTENT_URI,
